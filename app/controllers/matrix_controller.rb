b950d266ecf96f3e4cda9e73f0ca0dcf00d04608
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-06-10 19:11:54 -0400

    Adding workout page features.

--- app/controllers/matrix_controller.rb
+++ app/controllers/matrix_controller.rb
@@ -31,40 +31,11 @@ class MatrixController < ApplicationController
     @result = []
     @matrixChoices = []
 
-    
-
     $quizzes.each do |quiz|
       @data['exercises'].each do | exercise|
       id = quiz.id
       
-      # if (params["q#{id}".to_sym] == "upper")
-      # @result.push()["option_#{params["q#{id}".to_sym]}"])
-
       end
     end
   end
-    # $quizzes = Exercise.where(bodypart:params[:bodypart], difficulty:params[:difficulty])
-    # @result = []
-    # @matrixchoices = []
-    # @correct = []
-  
-      # if (params["q#{id}".to_sym] == "upper")
-    #   $quizzes.each do |quiz|
-
-    # # $matrixchoices.each do |exercise| 
-		#  @data['exercises'].each do | exercise|	
-		# 	id = exercise['exercise']['pk']
-		# 	@choice = Exercise.find(params["pk#{id}".to_sym]).choice
-		# 	if (@choice == params["q#{id}".to_sym]) 
-		# 		@matrixchoices.push(Exercise.find(params["pk#{id}".to_sym]).name)
-
-		# 		@result.push(Exercise.find(params["pk#{id}".to_sym])["option_#{params["q#{id}".to_sym]}"])
-		# 		@correct.push(true)
-	
-		# 	else
-		# 		@matrixchoices.push(Exercise.find(params["pk#{id}".to_sym]).description)
-		# 		@result.push(Exercise.find(params["pk#{id}".to_sym])["option_#{params["q#{id}".to_sym]}"] )
-		# 		@correct.push(false)
-		  
-
 end
